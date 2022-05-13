298e8545275cc10bf0e4931584820ffb8480d0ac
sangwon <paprika@consalad.io>
2022-05-13 17:41:22 +0900

    send message to webview

--- src/url.tsx
+++ src/url.tsx
@@ -4,11 +4,28 @@ import type { LinkProps } from "types";
 export const urlRegex =
   /(https?:\/\/|www\.)([-\w.]+\/[\p{L}\p{Emoji}\p{Emoji_Component}!#$%&'"()*+,./\\:;=_?@[\]~-]*[^\s'",.;:\b)\]}?]|(([\w-]+\.)+[\w-]+[\w/-]))/u;
 
-export const UrlComponent: React.FC<LinkProps> = ({ match: url, className }) => {
+export const UrlComponent: React.FC<LinkProps> = ({
+  match: url,
+  className,
+}) => {
+  const sendMessageToWebview = (url) => {
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({
+          type: "URL_OPEN",
+          url,
+        })
+      );
+    }
+  };
   return (
     <a
       className={className}
-      href={/^www\./.exec(url) ? `http://${url}` : url}
+      // href={/^www\./.exec(url) ? `http://${url}` : url}
+      // onClick={}
+      onClick={() =>
+        sendMessageToWebview(/^www\./.exec(url) ? `http://${url}` : url)
+      }
       target="_blank"
       rel="noreferrer"
     >
